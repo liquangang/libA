a1a01010370bb7a6364ab9b5dfab2ff7ea8edea4
liquangang <liquangang@idstaff.com>
2017-04-10 12:09:49 +0800

    提交项目代码

---
+++ libA/libA.h
@@ -0,0 +1,19 @@
+//
+//  libA.h
+//  libA
+//
+//  Created by quangang on 2017/4/10.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libA.
+FOUNDATION_EXPORT double libAVersionNumber;
+
+//! Project version string for libA.
+FOUNDATION_EXPORT const unsigned char libAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libA/PublicHeader.h>
+
+
