083e6cd6a55dded0de3343e976457e6b24c3e366
liquangang <liquangang@idstaff.com>
2017-04-10 12:37:02 +0800

    提交第一个测试方法

---
+++ libA/LibATestObj.h
@@ -0,0 +1,15 @@
+//
+//  LibATestObj.h
+//  libA
+//
+//  Created by quangang on 2017/4/10.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibATestObj : NSObject
+
+- (void)LibATestObjFirstMethod;
+
+@end
---
+++ libA/LibATestObj.m
@@ -0,0 +1,19 @@
+//
+//  LibATestObj.m
+//  libA
+//
+//  Created by quangang on 2017/4/10.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import "LibATestObj.h"
+
+@implementation LibATestObj
+
+- (void)LibATestObjFirstMethod{
+    NSLog(@"LibATestObj测试方法1");
+    NSLog(@"LibATestObj测试方法1");
+    NSLog(@"LibATestObj测试方法1");
+}
+
+@end
